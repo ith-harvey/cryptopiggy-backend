291705e1bec828d7cf6732d1011f5cac44550d8e
Ian Harvey <ith.harvey@gmail.com>
2017-11-13 14:33:52 -0600

    finally solved time problem, backend works

--- routes/controllers/utilfolder/dataClean.js
+++ routes/controllers/utilfolder/dataClean.js
@@ -6,9 +6,11 @@ const moment = require('moment');
 // 2 24 hours
 
 function windowPerform(data, maxTimeWindow, comparisonDaysVsHours) {
+  console.log(' /// //// ///////the function is run')
   let snapshotTime
   return data.reduce( (acum, priceHistObj) => {
     snapshotTime = Time.reformat(priceHistObj.created_at)
+    console.log('should be unique',snapshotTime)
 
     // initialize the object
     if (!acum.valueBackThen) acum.valueBackThen = null
@@ -17,19 +19,17 @@ function windowPerform(data, maxTimeWindow, comparisonDaysVsHours) {
 
     // if the maxTimewindow === the created_at price window set value
     if (moment(snapshotTime).isSame(maxTimeWindow)) {
-      // console.log('snapshottime', snapshotTime)
-      // console.log('maxTimeWindow', maxTimeWindow)
       acum.valueBackThen = priceHistObj.portfolio_value
     }
 
-    // if the maxTimewindow <= created_at price window, push into arr
+    // if the maxTimewindow is before created_at price window, push into arr
     if (moment(maxTimeWindow).isBefore(snapshotTime)) {
 
       if (comparisonDaysVsHours === undefined) {
         comparisonDaysVsHours = () => (Time.justTime(snapshotTime) === '12:00:00')
       }
 
-      if (comparisonDaysVsHours()) {
+      if (comparisonDaysVsHours()) { //return arr filled with days or hours
 
         acum.windowData.push({
             day: snapshotTime,
--- routes/controllers/utilfolder/time.js
+++ routes/controllers/utilfolder/time.js
@@ -15,37 +15,37 @@ class Time {
   static aYearAgo() {
     this.setup()
     let yearAgo = ((this.today.getFullYear()-1)+'-'+(this.today.getMonth()+1)+'-'+this.today.getDate());
-    return moment(yearAgo, 'YYYY-MM-DD 12:00:00').format('MM/DD/YYYY hh:mm:ss');
+    return moment(yearAgo, 'YYYY-MM-DD 12:00:00').format('MM/DD/YYYY HH:mm:ss');
   }
 
   static oneWeekAgo() {
     this.setup(+new Date - 604800000)
     let oneWeekAgo = ((this.today.getFullYear())+'-'+(this.today.getMonth()+1)+'-'+this.today.getDate());
-    return moment(oneWeekAgo, 'YYYY-MM-DD 12:00:00').format('MM/DD/YYYY hh:mm:ss');
+    return moment(oneWeekAgo, 'YYYY-MM-DD 12:00:00').format('MM/DD/YYYY HH:mm:ss');
   }
 
   static aDayAgo() {
     this.setup(+new Date - 864e5)
     let aDayAgo = ((this.today.getFullYear())+'-'+(this.today.getMonth()+1)+'-'+this.today.getDate()+' '+this.today.getHours()+':'+this.today.getMinutes()+':'+this.today.getSeconds());
 
-    return moment(aDayAgo, 'YYYY-MM-DD hh:mm:ss').startOf('hour').format('MM/DD/YYYY hh:mm:ss');
+    return moment(aDayAgo, 'YYYY-MM-DD HH:mm:ss').startOf('hour').format('MM/DD/YYYY HH:mm:ss');
   }
 
   static oneMonthAgo() {
     this.setup()
     let oneMonthDate = ((this.today.getFullYear())+'-'+(this.today.getMonth())+'-'+this.today.getDate());
-    return moment(oneMonthDate, 'YYYY-MM-DD 12:00:00').format('MM/DD/YYYY hh:mm:ss');
+    return moment(oneMonthDate, 'YYYY-MM-DD 12:00:00').format('MM/DD/YYYY HH:mm:ss');
   }
 
   static sixMonthsAgo() {
     this.setup()
     let sixMonthDate = ((this.today.getFullYear())+'-'+(this.today.getMonth()-5)+'-'+this.today.getDate());
-    return moment(sixMonthDate, 'YYYY-MM-DD 12:00:00').format('MM/DD/YYYY hh:mm:ss');
+    return moment(sixMonthDate, 'YYYY-MM-DD 12:00:00').format('MM/DD/YYYY HH:mm:ss');
   }
 
   static reformat(date) {
     date = date.toString().split(' ').splice(1,4).join(' ')
-    return moment(date, "MMM DD YYYY hh:mm:ss").format('MM/DD/YYYY hh:mm:ss')
+    return moment(date, "MMM DD YYYY HH:mm:ss").format('MM/DD/YYYY HH:mm:ss')
   }
 
   static justTime(date) {
