a081e873d5dba998b2743801e7e6c039dbbc5de1
Ian Harvey <ith.harvey@gmail.com>
2017-11-29 13:24:30 -0600

    setting up webhook

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.listen(port, function(error) {
   if (error) {
     console.error(error)
   } else {
+    console.log('//// //// /// //// // it updated!')
     console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
   }
 })
