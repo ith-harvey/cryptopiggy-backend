a3b86afa67d87b53044ea9fbe866b8ae0eaa144c
Ian Harvey <ith.harvey@gmail.com>
2017-11-28 18:17:35 -0600

    working on .env

--- db/performance-history.js
+++ db/performance-history.js
@@ -6,7 +6,7 @@ const options = {
   // Initialization Options
   promiseLib: promise
 };
-
+console.log('what we need', process.env.POSTGRESQL_USERANDPASS)
 const pgp = require('pg-promise')(options);
 const connectionString = `"postgresql://${process.env.POSTGRESQL_USERANDPASS}@cryptopiggy.chxcs4xgieuk.us-west-1.rds.amazonaws.com:5432/crypto_piggy";`
 const db = pgp(connectionString);
