c21e89f5e78b8ad0c61dfff13752e3302b4d9672
Ian Harvey <ith.harvey@gmail.com>
2017-12-11 15:33:51 -0600

    updated

--- routes/controllers/utilfolder/dataClean.js
+++ routes/controllers/utilfolder/dataClean.js
@@ -20,8 +20,6 @@ function windowPerform(data, maxTimeWindow, comparisonDaysVsHours) {
 
 
     // if the maxTimewindow === the created_at price window set value
-    // console.log('weekly result maxt window', maxTimeWindow)
-    // console.log('weekly result sn time', snapshotTime)
     if (moment(snapshotTime).isSame(maxTimeWindow)) {
       acum.valueBackThen = priceHistObj.portfolio_value
     }
