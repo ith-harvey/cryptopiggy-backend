0f2ff5d0c377b3e09cf4ac6696e9293d22491615
Ian Harvey <ith.harvey@gmail.com>
2017-12-12 19:08:24 -0600

    working on setting up email sending

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const { auth: ctrl } = require('./controllers')
 
 router.post('/signin', ctrl.signIn );
 router.post('/signup', ctrl.signUp );
+router.post('/sendreset', ctrl.sendPasswordReset );
 
 router.get('/', function (req,res,next) {
   res.send('Welcome to auth!')
